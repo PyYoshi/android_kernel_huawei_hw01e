5e9d7d27c9646722b2a4bf4b083030acbf312536
Yoshishiro Misawa <misawa@sackle.co.jp>
2013-12-26 11:30:06 +0900

    Revert "ARM: msm: fix compilation flags for MSM_SCM"
    This reverts commit fe7db0b5cbd636ef9e8ae983db484f9bc9e13250.

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -173,9 +173,6 @@ static u32 smc(u32 cmd_addr)
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
 			__asmeq("%3", "r2")
-#ifdef REQUIRES_SEC
-                        ".arch_extension sec\n"
-#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0)
 			: "r" (r0), "r" (r1), "r" (r2)
@@ -297,9 +294,6 @@ s32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
-#ifdef REQUIRES_SEC
-                        ".arch_extension sec\n"
-#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2)
@@ -332,9 +326,6 @@ s32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
-#ifdef REQUIRES_SEC
-                        ".arch_extension sec\n"
-#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3));
@@ -362,9 +353,6 @@ s32 scm_call_atomic4_3(u32 svc, u32 cmd, u32 arg1, u32 arg2,
 		__asmeq("%4", "r1")
 		__asmeq("%5", "r2")
 		__asmeq("%6", "r3")
-#ifdef REQUIRES_SEC
-                        ".arch_extension sec\n"
-#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0), "=r" (r1), "=r" (r2)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3), "r" (r4), "r" (r5));
@@ -397,9 +385,6 @@ u32 scm_get_version(void)
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
-#ifdef REQUIRES_SEC
-                        ".arch_extension sec\n"
-#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0), "=r" (r1)
 			: "r" (r0), "r" (r1)
